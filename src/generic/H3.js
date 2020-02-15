b59d3359e2a051ee0b9cb857d942b5328dcb7cbc
1gory <igor.pershin.me@gmail.com>
2020-02-15 12:07:14 +0300

    test

--- src/generic/H3.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.h3`
-  margin-bottom: 5px;
-  padding-bottom: 3px;
-  font-size: 22px;
-  cursor: pointer;
-  margin: 0;
-`;
--- src/generic/P.js
+++
@@ -1,7 +0,0 @@
-import styled, { keyframes } from 'styled-components';
-
-export default styled.p`
-  height: ${props => (props.isOpen ? '100%' : 0)};
-  margin-top: 10px;
-  overflow: hidden;
-`;
